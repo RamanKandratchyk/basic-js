ebc230a675d89ba62f15bca3b82bea887e0a7d16
Raman Kandratchyk <kondrator@gmail.com>
2022-08-30 15:35:49 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,87 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction === undefined ? true : direction;
+    this.alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let spaceArr = [], countSpaces = 0;
+    message.split('').forEach((el, i) => {
+      if (el === ' ') {
+        spaceArr.push(i - countSpaces);
+        countSpaces++;
+      };
+    });
+
+    message = message.toUpperCase().split(' ').join('');
+    key = key.toUpperCase();
+
+    let result = '';
+
+    for (let i = 0; i < message.length; i++) {
+      let messI = this.alpha.indexOf(message[i]);
+
+      if (messI === -1) {
+        this.c = message[i];
+      } else {
+        let keyI_s = key[((i >= key.length) ? i % key.length : i)];
+        let keyI = this.alpha.indexOf(keyI_s);
+        this.c = this.alpha[(((this.alpha.length + (messI + keyI)) % this.alpha.length))];
+      };
+
+      result += this.c;
+    };
+
+    let resultSpaces = [];
+    result.split('').forEach((el, i) => {
+      if (spaceArr.includes(i)) resultSpaces.push(' ');
+      resultSpaces.push(el);
+    });
+
+    return this.direction ? resultSpaces.join('') : resultSpaces.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let spaceArr = [], countSpaces = 0;
+    message.split('').forEach((el, i) => {
+      if (el === ' ') {
+        spaceArr.push(i - countSpaces);
+        countSpaces++;
+      };
+    });
+
+    message = message.toUpperCase().split(' ').join('');
+    key = key.toUpperCase();
+
+    let result = '';
+
+    for (let i = 0; i < message.length; i++) {
+      let messI = this.alpha.indexOf(message[i]);
+
+      if (messI === -1) {
+        this.c = message[i];
+      } else {
+        let keyI_s = key[((i >= key.length) ? i % key.length : i)];
+        let keyI = this.alpha.indexOf(keyI_s);
+        this.c = this.alpha[(((this.alpha.length + (messI - keyI)) % this.alpha.length))];
+      };
+
+      result += this.c;
+    };
+
+    let resultSpaces = [];
+    result.split('').forEach((el, i) => {
+      if (spaceArr.includes(i)) resultSpaces.push(' ');
+      resultSpaces.push(el);
+    });
+
+    return this.direction ? resultSpaces.join('') : resultSpaces.reverse().join('');
   }
 }
 
